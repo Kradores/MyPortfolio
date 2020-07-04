7b264ba9e70044f19eab207ceefce551f3fb2cb0
Alexandru Noroc <kradores10@gmail.com>
2020-07-05 01:04:50 +0300

    UPDATED: nav tabs animation

--- js/main.js
+++ js/main.js
@@ -1,34 +1,69 @@
-const navMenu = document.querySelectorAll("ul.nav-items");
-const navItems = navMenu[0].children;
+function prepareNavItems() {
+	var scrollAnimationElements = {};
 
-for (const navItemKey in navItems) {
-	const navItem = navItems[navItemKey];
-	if (typeof navItem !== "object") {
-		continue;
+	const navItems = document.querySelectorAll("ul.nav-items li");
+	const sections = document.querySelectorAll("section");
+
+	for (const key in navItems) {
+		if (isNaN(key)) {
+			continue;
+		}
+		scrollAnimationElements[key] = {
+			navItem: navItems[key],
+			section: sections[key],
+		};
+		scrollAnimationElements.length = navItems.length;
 	}
-	navItem.children[0].addEventListener("click", function () {
-		if (navItem.classList.contains("active") || navItem.classList.contains("active-reverse")) {
-			return;
+
+	scrollAnimationElements.getBounds = function () {
+		for (const key in this) {
+			if (isNaN(key)) {
+				continue;
+			}
+			this[key].bounds = this[key].section.getBoundingClientRect();
+		}
+	};
+
+	return scrollAnimationElements;
+}
+
+const scrollAnimationElements = prepareNavItems();
+
+const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+var scrollPos = 0;
+
+window.addEventListener("scroll", function () {
+	scrollAnimationElements.getBounds();
+
+	for (const key in scrollAnimationElements) {
+		if (isNaN(key)) {
+			continue;
 		}
 
-		for (const itemKey in navItems) {
+		if (
+			scrollAnimationElements[key].bounds.bottom > viewportHeight * 0.6 &&
+			scrollAnimationElements[key].bounds.bottom < viewportHeight * 1.4
+		) {
 			if (
-				navItems[itemKey].classList !== undefined &&
-				(navItems[itemKey].classList.contains("active") ||
-					navItems[itemKey].classList.contains("active-reverse")) &&
-				navItemKey > itemKey
+				scrollAnimationElements[key].navItem.classList.contains("active") ||
+				scrollAnimationElements[key].navItem.classList.contains("active-reverse")
 			) {
-				navItem.classList = "active";
-				navItems[itemKey].classList = "deactivate";
-			} else if (
-				navItems[itemKey].classList !== undefined &&
-				(navItems[itemKey].classList.contains("active") ||
-					navItems[itemKey].classList.contains("active-reverse")) &&
-				navItemKey < itemKey
-			) {
-				navItem.classList = "active-reverse";
-				navItems[itemKey].classList = "deactivate-reverse";
+				continue;
+			}
+
+			if (document.body.getBoundingClientRect().top < scrollPos) {
+				if (key > 0) {
+					scrollAnimationElements[+key - 1].navItem.classList = "deactivate";
+					scrollAnimationElements[key].navItem.classList = "active";
+				}
+			} else {
+				if (key < scrollAnimationElements.length - 1) {
+					scrollAnimationElements[key].navItem.classList = "active-reverse";
+					scrollAnimationElements[+key + 1].navItem.classList = "deactivate-reverse";
+				}
 			}
 		}
-	});
-}
+	}
+
+	scrollPos = document.body.getBoundingClientRect().top;
+});
